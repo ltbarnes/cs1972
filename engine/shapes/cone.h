06b287bcdc0285591e037a46fde54c686df7b9e1
barneslt63@gmail.com <logan_barnes@brown.edu>
2015-02-03 18:19:37 -0500

    added cone class

--- engine/shapes/cone.h
+++ engine/shapes/cone.h
@@ -7,6 +7,13 @@ class Cone : public Shape
 {
 public:
     Cone(int complexity);
+    virtual ~Cone();
+
+    virtual void calcVerts();
+
+protected:
+    virtual void make3DSlice(int *index, glm::vec2 left, glm::vec2 right, float normSlope, float leftU, float rightU);
+    void makeBottomSlice(int *index, glm::vec3 left, glm::vec3 right);
 };
 
 #endif // CONE_H
